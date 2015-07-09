0fa2d73367b58fb875ba69cd94c1975f4bcfa79b
aanupam3 <aditya.anupam91@gmail.com>
2015-07-09 01:22:26 -0400

    Added Cabbage Thrower basic

---
+++ Assets/Scripts/CabbageThrow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CabbageThrow : MonoBehaviour {
+	float cabbage_force = 1000;
+
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	void ThrowCabbage()
+	{
+		Vector3 throwPosition = transform.position;
+		throwPosition.Set(transform.position.x,transform.position.y+5,transform.position.z);
+		Debug.Log (throwPosition);
+		GameObject cabbage = (GameObject)GameObject.Instantiate(Resources.Load("Food_Watermelon"),throwPosition,Quaternion.identity);
+		Debug.Log ("Created");
+		Rigidbody cabbage_body = cabbage.GetComponent<Rigidbody>();
+		cabbage_body.AddForce(transform.forward * cabbage_force);
+	
+
+	}
+}
