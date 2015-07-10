2311e438248431a4faaa9503ee90b32c432b0893
aanupam3 <aditya.anupam91@gmail.com>
2015-07-09 20:56:54 -0400

    Cop Sounds

---
+++ Assets/Scripts/CopSound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopSound : MonoBehaviour {
+
+	public AudioClip[] copClips;
+	AudioSource copSource;
+	Animator anim;
+	static int i = 0;
+	// Use this for initialization
+	void Start () {
+		copSource = gameObject.AddComponent<AudioSource>();
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(anim.GetBool("run"))
+		{
+			copSource.clip = copClips[i];
+			if(!copSource.isPlaying)
+			{
+				copSource.PlayDelayed(2);
+			}
+			else
+			{
+				i = (i+1)%(copClips.Length);
+				Debug.Log (i);
+			}
+		}
+	}
+}
