ed1505a9861a858a6761134c3cdc58abf68bb697
Sabrina Greenlee <jkeum3@gatech.edu>
2015-07-12 00:22:27 -0400

    added derpy cop

---
+++ Assets/AI/Actions/DanceAction.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class DanceAction : RAINAction
+{
+    public override void Start(RAIN.Core.AI ai)
+	{
+		float danceTime = (float) ai.WorkingMemory.GetItem ("danceTime");
+		
+		danceTime += Time.deltaTime;
+		
+		//Debug.Log(danceTime);
+		
+		ai.WorkingMemory.SetItem ("danceTime", danceTime);
+
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+	{
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
