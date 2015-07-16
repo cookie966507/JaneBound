64f46192962f989ff09ab33027002de4a3a59138
aanupam3 <aditya.anupam91@gmail.com>
2015-07-16 15:21:30 -0400

    Basic Credits Background Scene

---
+++ Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Data;
+
+
+public class CreditsScript : MonoBehaviour {
+
+	// Use this for initialization
+	AudioSource bgmSource;
+	void Awake () {
+		bgmSource = gameObject.GetComponent<AudioSource>();
+	}
+	void Start () {
+		GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Animator>().SetBool("playerDance",true);
+		GameObject.FindGameObjectWithTag("CabbageGuy").GetComponent<Animator>().SetBool("dance",true);
+		GameObject.FindGameObjectWithTag("Janitor").GetComponent<Animator>().SetBool("dance",true);
+		GameObject.FindGameObjectWithTag("Enemy").GetComponent<Animator>().SetBool("dance2",true);
+
+		bgmSource.Play();
+		//SoundManager.PlayMusic(bgmSource);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
